59067db0f27043e12fc96aebb3f5a4ec3ec26381
Juan Jose Hernandez Garcia <juanhernandez@MacBook-Air-de-Juan.local>
2022-10-14 13:22:59 -0500

    :sparkles: all variables implemented

--- main.js
+++ main.js
@@ -3,3 +3,4 @@ console.log('hola por segunda vez')
 const newVariable = "esta es una nueva variable";
 const secondVariable = 'hello';
 const thirdVariable = 'hahaha quickly';
+const lastVariable = 'oooh';
