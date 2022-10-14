13549f21ef3fcd0ba20629a814305e7cde788fba
JuanDedica2001 <104235936+JuanDedica2001@users.noreply.github.com>
2022-10-14 13:29:06 -0500

    [CP-1]: First merge (#1)
    * :white_check_mark: third variable
    
    yes!!
    
    * :sparkles: all variables implemented
    
    Co-authored-by: Juan Jose Hernandez Garcia <juanhernandez@MacBook-Air-de-Juan.local>

--- main.js
+++ main.js
@@ -2,3 +2,5 @@ console.log('hola');
 console.log('hola por segunda vez')
 const newVariable = "esta es una nueva variable";
 const secondVariable = 'hello';
+const thirdVariable = 'hahaha quickly';
+const lastVariable = 'oooh';
